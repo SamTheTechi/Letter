64da6361bba203f23f18df3c7a1d536a4a7dd15a
SamTheProBot <gsameer478@gmail.com>
2024-01-02 18:57:49 +0530

    please work

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,14 @@
-import { defineConfig } from "vite";
+// import { defineConfig } from "vite";
+// import react from "@vitejs/plugin-react-swc";
+
+// export default defineConfig({
+//   plugins: [react()],
+//   base: "/Letter/",
+// });
+// vite.config.js;
 import react from "@vitejs/plugin-react-swc";
 
-export default defineConfig({
+export default {
   plugins: [react()],
   base: "/Letter/",
-});
-vite.config.js;
+};
