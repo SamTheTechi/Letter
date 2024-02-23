579ded02a78e1c460429b2b5cbb922fbd0415bcf
SamTheProBot <gsameer478@gmail.com>
2024-02-23 10:21:58 +0530

    UI updata

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,14 @@
 import React, { useEffect } from 'react';
 import Landing from './pages/landing';
 import Letter from './pages/letter';
-import { Routes, Route } from 'react-router-dom';
+import { Route, Routes } from 'react-router-dom';
 
 const App = () => {
   return (
     <section style={{ background: 'fixed', overflow: 'hidden' }}>
       <Routes>
-        <Route path={'/'} element={<Landing />}></Route>
-        <Route path={'/Slides'} element={<Letter />}></Route>
+        <Route path='/' element={<Landing />}></Route>
+        <Route path='/Slides' element={<Letter />}></Route>
       </Routes>
     </section>
   );
